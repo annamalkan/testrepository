dc9e34ebaa4c75115049d6954065782c6d038ece
annamalkan <annamalkan@gmail.com>
2015-08-05 10:56:04 +0200

    Implemented an Item class

--- robot_pricing.py
+++ robot_pricing.py
@@ -1,12 +1,17 @@
 #This is a test of Python with robot pricing
-robot = {
-	"price":900,
-	"count":2,
-	"vat":1.25
-}
-book={
-	"price":100,
-	"count":1,
-	"vat":1.06
-}
-print robot["price"]*robot["count"]*robot["vat"]+book["price"]*book["count"]*book["vat"]
+class Item(object):
+	price=0
+	count=0
+	vat=0
+
+robot = Item()
+robot.price=900
+robot.count=2
+robot.vat=1.25
+
+book = Item()
+book.price=100
+book.count=1
+book.vat=1.06
+
+print robot.price*robot.count*robot.vat+book.price*book.count*book.vat
