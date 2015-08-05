07e2a919eba06ffda4d7326a818a2b09f9618571
annamalkan <annamalkan@gmail.com>
2015-08-05 11:53:59 +0200

    Added discount to items over 500

--- robot_pricing.py
+++ robot_pricing.py
@@ -10,11 +10,21 @@ class Item(object):
 		self.vat=vat
 
 	def total_price(self):
-		return self.price*self.count*self.vat
+		full_price= self.price*self.count*self.vat
+		if self.price > 500:
+			return 0.9*full_price
+		else:
+			return full_price
 		
 items=[Item(900,2,1.25), Item(100,1,1.06)]
-total=items[0].total_price()+items[1].total_price()
-print total
+total=0
+
+for item in items:
+	total+=item.total_price()
+
+
+# total=items[0].total_price()+items[1].total_price()
+print "Your total with discount is", total, "SEK"
 #robot = Item(900,2,1.25)
 # robot.price=900
 # robot.count=2
