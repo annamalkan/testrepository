4e52fcad1a4389a5097b21f59d70bc001b0bd40d
annamalkan <annamalkan@gmail.com>
2015-08-05 11:23:05 +0200

    Implemented list of products

--- robot_pricing.py
+++ robot_pricing.py
@@ -4,19 +4,27 @@ class Item(object):
 	count=0
 	vat=0
 
+	def __init__(self,price,count,vat):
+		self.price=price
+		self.count=count
+		self.vat=vat
+
 	def total_price(self):
 		return self.price*self.count*self.vat
 		
-robot = Item()
-robot.price=900
-robot.count=2
-robot.vat=1.25
+items=[Item(900,2,1.25), Item(100,1,1.06)]
+total=items[0].total_price+items[1].total_price
+print total
+#robot = Item(900,2,1.25)
+# robot.price=900
+# robot.count=2
+# robot.vat=1.25
 
-book = Item()
-book.price=100
-book.count=1
-book.vat=1.06
+#book = Item(100,1,1.06)
+# book.price=100
+# book.count=1
+# book.vat=1.06
 
-print robot.total_price()+book.total_price()
+#print robot.total_price()+book.total_price()
 
 #print robot.price*robot.count*robot.vat+book.price*book.count*book.vat
